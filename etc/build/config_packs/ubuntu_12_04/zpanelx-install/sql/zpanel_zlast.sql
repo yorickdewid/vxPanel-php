0dae690c952bc24c87536c3be1f8af153d3b0768
Niels <kaas@kaas-VirtualBox.(none)>
2015-05-18 18:59:22 +0200

    fixed sql errors and missing db and security files

--- etc/build/config_packs/ubuntu_12_04/zpanelx-install/sql/zpanel_zlast.sql
+++ etc/build/config_packs/ubuntu_12_04/zpanelx-install/sql/zpanel_zlast.sql
@@ -1,6 +1,5 @@
 
 USE zpanel_core;
--- Structuur van  tabel test.credit_transaction wordt geschreven
 CREATE TABLE IF NOT EXISTS `x_credit_transaction` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `wallet_id` int(11) NOT NULL,
@@ -10,24 +9,16 @@ CREATE TABLE IF NOT EXISTS `x_credit_transaction` (
   PRIMARY KEY (`id`),
   KEY `wallet` (`wallet_id`),
   KEY `FK_credit_transaction_status` (`status_id`),
-  CONSTRAINT `FK_credit_transaction_status` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `wallet` FOREIGN KEY (`wallet_id`) REFERENCES `wallet` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
+  CONSTRAINT `FK_credit_transaction_status` FOREIGN KEY (`status_id`) REFERENCES `x_status` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `wallet` FOREIGN KEY (`wallet_id`) REFERENCES `x_wallet` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Data exporteren was gedeselecteerd
-
-
--- Structuur van  tabel test.status wordt geschreven
 CREATE TABLE IF NOT EXISTS `x_status` (
   `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   `status` char(20) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Data exporteren was gedeselecteerd
-
-
--- Structuur van  tabel test.wallet wordt geschreven
 CREATE TABLE IF NOT EXISTS `x_wallet` (
   `id` int(11) NOT NULL,
   `total` double NOT NULL DEFAULT '0',
@@ -42,5 +33,5 @@ insert into x_modules (`mo_category_fk`,`mo_name_vc`,`mo_version_in`,`mo_folder_
 insert into x_modules (`mo_category_fk`,`mo_name_vc`,`mo_version_in`,`mo_folder_vc`,`mo_type_en`,`mo_desc_tx`) VALUES (3,'Credit Funds','001','credits','user','With credits you can pay for your services.');
 update x_modules set `mo_folder_vc` = 'register' where mo_id_pk = 15;
 
-insert into x_status (`status`) VALUES("The transaction failed"); //id 0
-insert into x_status (`status`) VALUES("The transaction was succesful"); //id 1
+insert into x_status (`status`) VALUES("The transaction failed");
+insert into x_status (`status`) VALUES("The transaction was succesful");
