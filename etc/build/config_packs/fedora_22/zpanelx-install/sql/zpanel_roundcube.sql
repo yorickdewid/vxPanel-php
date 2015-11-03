6dc5640621bbb6f9f9853b721619bad3e6415aba
yorickdewid <yorick17@outlook.com>
2015-11-03 16:20:48 +0100

    Update SQL tables

--- etc/build/config_packs/fedora_22/zpanelx-install/sql/zpanel_roundcube.sql
+++ etc/build/config_packs/fedora_22/zpanelx-install/sql/zpanel_roundcube.sql
@@ -1,12 +1,14 @@
--- Custom Updater from 0.7 to 0.8.*
--- Runs in line with ZPanelX 10.0.0 to 10.0.1 update
-USE zpanel_roundcube;
+-- Roundcube Webmail initial database structure
 
-/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+
+/*!40014  SET FOREIGN_KEY_CHECKS=0 */;
+
+CREATE SCHEMA IF NOT EXISTS `zpanel_roundcube`;
+USE zpanel_roundcube;
 
 -- Table structure for table `session`
 
-CREATE TABLE IF NOT EXISTS `session` (
+CREATE TABLE `session` (
  `sess_id` varchar(128) NOT NULL,
  `created` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
  `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
@@ -19,30 +21,26 @@ CREATE TABLE IF NOT EXISTS `session` (
 
 -- Table structure for table `users`
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
  `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
  `username` varchar(128) BINARY NOT NULL,
  `mail_host` varchar(128) NOT NULL,
- `alias` varchar(128) BINARY NOT NULL,
  `created` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
  `last_login` datetime DEFAULT NULL,
  `language` varchar(5),
  `preferences` text,
  PRIMARY KEY(`user_id`),
- UNIQUE `username` (`username`, `mail_host`),
- INDEX `alias_index` (`alias`)
+ UNIQUE `username` (`username`, `mail_host`)
 ) /*!40000 ENGINE=INNODB */ /*!40101 CHARACTER SET utf8 COLLATE utf8_general_ci */;
 
 
 -- Table structure for table `cache`
 
-CREATE TABLE IF NOT EXISTS `cache` (
- `cache_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `cache` (
+ `user_id` int(10) UNSIGNED NOT NULL,
  `cache_key` varchar(128) /*!40101 CHARACTER SET ascii COLLATE ascii_general_ci */ NOT NULL ,
  `created` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
  `data` longtext NOT NULL,
- `user_id` int(10) UNSIGNED NOT NULL,
- PRIMARY KEY(`cache_id`),
  CONSTRAINT `user_id_fk_cache` FOREIGN KEY (`user_id`)
    REFERENCES `users`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
  INDEX `created_index` (`created`),
@@ -52,7 +50,7 @@ CREATE TABLE IF NOT EXISTS `cache` (
 
 -- Table structure for table `cache_index`
 
-CREATE TABLE IF NOT EXISTS `cache_index` (
+CREATE TABLE `cache_index` (
  `user_id` int(10) UNSIGNED NOT NULL,
  `mailbox` varchar(255) BINARY NOT NULL,
  `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
@@ -67,7 +65,7 @@ CREATE TABLE IF NOT EXISTS `cache_index` (
 
 -- Table structure for table `cache_thread`
 
-CREATE TABLE IF NOT EXISTS `cache_thread` (
+CREATE TABLE `cache_thread` (
  `user_id` int(10) UNSIGNED NOT NULL,
  `mailbox` varchar(255) BINARY NOT NULL,
  `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
@@ -81,7 +79,7 @@ CREATE TABLE IF NOT EXISTS `cache_thread` (
 
 -- Table structure for table `cache_messages`
 
-CREATE TABLE IF NOT EXISTS `cache_messages` (
+CREATE TABLE `cache_messages` (
  `user_id` int(10) UNSIGNED NOT NULL,
  `mailbox` varchar(255) BINARY NOT NULL,
  `uid` int(11) UNSIGNED NOT NULL DEFAULT '0',
@@ -97,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `cache_messages` (
 
 -- Table structure for table `contacts`
 
-CREATE TABLE IF NOT EXISTS `contacts` (
+CREATE TABLE `contacts` (
  `contact_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
  `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
  `del` tinyint(1) NOT NULL DEFAULT '0',
@@ -116,7 +114,7 @@ CREATE TABLE IF NOT EXISTS `contacts` (
 
 -- Table structure for table `contactgroups`
 
-CREATE TABLE IF NOT EXISTS `contactgroups` (
+CREATE TABLE `contactgroups` (
   `contactgroup_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` int(10) UNSIGNED NOT NULL,
   `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
@@ -128,7 +126,7 @@ CREATE TABLE IF NOT EXISTS `contactgroups` (
   INDEX `contactgroups_user_index` (`user_id`,`del`)
 ) /*!40000 ENGINE=INNODB */ /*!40101 CHARACTER SET utf8 COLLATE utf8_general_ci */;
 
-CREATE TABLE IF NOT EXISTS `contactgroupmembers` (
+CREATE TABLE `contactgroupmembers` (
   `contactgroup_id` int(10) UNSIGNED NOT NULL,
   `contact_id` int(10) UNSIGNED NOT NULL,
   `created` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
@@ -143,7 +141,7 @@ CREATE TABLE IF NOT EXISTS `contactgroupmembers` (
 
 -- Table structure for table `identities`
 
-CREATE TABLE IF NOT EXISTS `identities` (
+CREATE TABLE `identities` (
  `identity_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
  `user_id` int(10) UNSIGNED NOT NULL,
  `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
@@ -159,13 +157,14 @@ CREATE TABLE IF NOT EXISTS `identities` (
  PRIMARY KEY(`identity_id`),
  CONSTRAINT `user_id_fk_identities` FOREIGN KEY (`user_id`)
    REFERENCES `users`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
- INDEX `user_identities_index` (`user_id`, `del`)
+ INDEX `user_identities_index` (`user_id`, `del`),
+ INDEX `email_identities_index` (`email`, `del`)
 ) /*!40000 ENGINE=INNODB */ /*!40101 CHARACTER SET utf8 COLLATE utf8_general_ci */;
 
 
 -- Table structure for table `dictionary`
 
-CREATE TABLE IF NOT EXISTS `dictionary` (
+CREATE TABLE `dictionary` (
   `user_id` int(10) UNSIGNED DEFAULT NULL,
   `language` varchar(5) NOT NULL,
   `data` longtext NOT NULL,
@@ -177,7 +176,7 @@ CREATE TABLE IF NOT EXISTS `dictionary` (
 
 -- Table structure for table `searches`
 
-CREATE TABLE IF NOT EXISTS `searches` (
+CREATE TABLE `searches` (
  `search_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
  `user_id` int(10) UNSIGNED NOT NULL,
  `type` int(3) NOT NULL DEFAULT '0',
@@ -190,25 +189,14 @@ CREATE TABLE IF NOT EXISTS `searches` (
 ) /*!40000 ENGINE=INNODB */ /*!40101 CHARACTER SET utf8 COLLATE utf8_general_ci */;
 
 
-/*!40014 SET FOREIGN_KEY_CHECKS=1 */;
+-- Table structure for table `system`
 
-/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
-
-ALTER TABLE `contacts` DROP FOREIGN KEY `user_id_fk_contacts`;
-ALTER TABLE `contacts` DROP INDEX `user_contacts_index`;
-ALTER TABLE `contacts` MODIFY `email` text NOT NULL;
-ALTER TABLE `contacts` ADD INDEX `user_contacts_index` (`user_id`,`del`);
-ALTER TABLE `contacts` ADD CONSTRAINT `user_id_fk_contacts` FOREIGN KEY (`user_id`)
-REFERENCES `users`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE `cache` ALTER `user_id` DROP DEFAULT;
-ALTER TABLE `cache_index` ALTER `user_id` DROP DEFAULT;
-ALTER TABLE `cache_thread` ALTER `user_id` DROP DEFAULT;
-ALTER TABLE `cache_messages` ALTER `user_id` DROP DEFAULT;
-ALTER TABLE `contacts` ALTER `user_id` DROP DEFAULT;
-ALTER TABLE `contactgroups` ALTER `user_id` DROP DEFAULT;
-ALTER TABLE `contactgroupmembers` ALTER `contact_id` DROP DEFAULT;
-ALTER TABLE `identities` ALTER `user_id` DROP DEFAULT;
-ALTER TABLE `searches` ALTER `user_id` DROP DEFAULT;
+CREATE TABLE `system` (
+ `name` varchar(64) NOT NULL,
+ `value` mediumtext,
+ PRIMARY KEY(`name`)
+) /*!40000 ENGINE=INNODB */ /*!40101 CHARACTER SET utf8 COLLATE utf8_general_ci */;
 
 /*!40014 SET FOREIGN_KEY_CHECKS=1 */;
+
+INSERT INTO system (name, value) VALUES ('roundcube-version', '2013011700');
